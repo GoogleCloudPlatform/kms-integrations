adefa267c07898c71ef03b1304d7e0700826afd3
Bradley Hess <bdhess@google.com>
2020-05-21 15:59:39 -0400

    Add data structure for storing PKCS #11 attributes
    Bug: b/148377771
    Change-Id: I6a32bb74a9e58b7f051d35fc31f631843f59c85e

---
+++ kmsp11/attribute_map.cc
@@ -0,0 +1,46 @@
+#include "kmsp11/attribute_map.h"
+
+#include "kmsp11/util/errors.h"
+
+namespace kmsp11 {
+
+void AttributeMap::Put(CK_ATTRIBUTE_TYPE type, absl::string_view value) {
+  attrs_[type] = std::string(value.data(), value.size());
+}
+
+void AttributeMap::PutSensitive(CK_ATTRIBUTE_TYPE type) {
+  static constexpr SensitiveValue kSensitiveValue;
+  attrs_[type] = kSensitiveValue;
+}
+
+bool AttributeMap::Contains(const CK_ATTRIBUTE& attribute) const {
+  auto it = attrs_.find(attribute.type);
+  if (it == attrs_.end()) {
+    return false;
+  }
+  if (absl::holds_alternative<SensitiveValue>(it->second)) {
+    return false;
+  }
+
+  return absl::get<std::string>(it->second) ==
+         absl::string_view(static_cast<char*>(attribute.pValue),
+                           attribute.ulValueLen);
+}
+
+StatusOr<absl::string_view> AttributeMap::Value(CK_ATTRIBUTE_TYPE type) const {
+  auto it = attrs_.find(type);
+  if (it == attrs_.end()) {
+    return NewError(absl::StatusCode::kNotFound,
+                    absl::StrFormat("attribute not found: %#x", type),
+                    CKR_ATTRIBUTE_TYPE_INVALID, SOURCE_LOCATION);
+  }
+  if (absl::holds_alternative<SensitiveValue>(it->second)) {
+    return NewError(absl::StatusCode::kPermissionDenied,
+                    absl::StrFormat("attribute value sensitive: %#x", type),
+                    CKR_ATTRIBUTE_SENSITIVE, SOURCE_LOCATION);
+  }
+  const std::string& s = absl::get<std::string>(it->second);
+  return absl::string_view(s);
+}
+
+}  // namespace kmsp11
