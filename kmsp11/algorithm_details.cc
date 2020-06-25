b8ecd11b0946bdf78946b1bf0db8855fa8e46054
Bradley Hess <bdhess@google.com>
2020-06-25 16:30:46 -0400

    Cleanup: remove custom macros
    Change-Id: I06e5daac619c392c1faef789b0966ed746ad1f60

--- kmsp11/algorithm_details.cc
+++ kmsp11/algorithm_details.cc
@@ -5,176 +5,171 @@
 
 namespace kmsp11 {
 
-#define KMS_ALGORITHM(name) \
-  kms_v1::CryptoKeyVersion_CryptoKeyVersionAlgorithm_##name
-#define KMS_PURPOSE(name) kms_v1::CryptoKey_CryptoKeyPurpose_##name
+static const auto* kAlgorithmDetails = new absl::flat_hash_map<
+    kms_v1::CryptoKeyVersion_CryptoKeyVersionAlgorithm, AlgorithmDetails>{
 
-static const auto* kAlgorithmDetails =
-    new absl::flat_hash_map<kms_v1::CryptoKeyVersion_CryptoKeyVersionAlgorithm,
-                            AlgorithmDetails>{
+    // EC_SIGN_*
+    {kms_v1::CryptoKeyVersion::EC_SIGN_P256_SHA256,
+     AlgorithmDetails{
+         kms_v1::CryptoKeyVersion::EC_SIGN_P256_SHA256,  // algorithm
+         kms_v1::CryptoKey::ASYMMETRIC_SIGN,             // purpose
+         {CKM_ECDSA},                                    // allowed_mechanisms
+         CKK_EC,                                         // key_type
+         256,                                            // key_size
+         CKM_EC_KEY_PAIR_GEN,                            // key_gen_mechanism
+         CKM_SHA256,                                     // digest_mechanism
+         EVP_sha256(),                                   // digest
+     }},
+    {kms_v1::CryptoKeyVersion::EC_SIGN_P384_SHA384,
+     AlgorithmDetails{
+         kms_v1::CryptoKeyVersion::EC_SIGN_P384_SHA384,  // algorithm
+         kms_v1::CryptoKey::ASYMMETRIC_SIGN,             // purpose
+         {CKM_ECDSA},                                    // allowed_mechanisms
+         CKK_EC,                                         // key_type
+         384,                                            // key_size
+         CKM_EC_KEY_PAIR_GEN,                            // key_gen_mechanism
+         CKM_SHA384,                                     // digest_mechanism
+         EVP_sha384(),                                   // digest
+     }},
 
-        // EC_SIGN_*
-        {KMS_ALGORITHM(EC_SIGN_P256_SHA256),
-         AlgorithmDetails{
-             KMS_ALGORITHM(EC_SIGN_P256_SHA256),  // algorithm
-             KMS_PURPOSE(ASYMMETRIC_SIGN),        // purpose
-             {CKM_ECDSA},                         // allowed_mechanisms
-             CKK_EC,                              // key_type
-             256,                                 // key_size
-             CKM_EC_KEY_PAIR_GEN,                 // key_gen_mechanism
-             CKM_SHA256,                          // digest_mechanism
-             EVP_sha256(),                        // digest
-         }},
-        {KMS_ALGORITHM(EC_SIGN_P384_SHA384),
-         AlgorithmDetails{
-             KMS_ALGORITHM(EC_SIGN_P384_SHA384),  // algorithm
-             KMS_PURPOSE(ASYMMETRIC_SIGN),        // purpose
-             {CKM_ECDSA},                         // allowed_mechanisms
-             CKK_EC,                              // key_type
-             384,                                 // key_size
-             CKM_EC_KEY_PAIR_GEN,                 // key_gen_mechanism
-             CKM_SHA384,                          // digest_mechanism
-             EVP_sha384(),                        // digest
-         }},
+    // RSA_DECRYPT_OAEP_*
+    {kms_v1::CryptoKeyVersion::RSA_DECRYPT_OAEP_2048_SHA256,
+     AlgorithmDetails{
+         kms_v1::CryptoKeyVersion::RSA_DECRYPT_OAEP_2048_SHA256,  // algorithm
+         kms_v1::CryptoKey::ASYMMETRIC_DECRYPT,                   // purpose
+         {CKM_RSA_PKCS_OAEP},        // allowed_mechanisms
+         CKK_RSA,                    // key_type
+         2048,                       // key_size
+         CKM_RSA_PKCS_KEY_PAIR_GEN,  // key_gen_mechanism
+         CKM_SHA256,                 // digest_mechanism
+         EVP_sha256(),               // digest
+     }},
+    {kms_v1::CryptoKeyVersion::RSA_DECRYPT_OAEP_3072_SHA256,
+     AlgorithmDetails{
+         kms_v1::CryptoKeyVersion::RSA_DECRYPT_OAEP_3072_SHA256,  // algorithm
+         kms_v1::CryptoKey::ASYMMETRIC_DECRYPT,                   // purpose
+         {CKM_RSA_PKCS_OAEP},        // allowed_mechanisms
+         CKK_RSA,                    // key_type
+         3072,                       // key_size
+         CKM_RSA_PKCS_KEY_PAIR_GEN,  // key_gen_mechanism
+         CKM_SHA256,                 // digest_mechanism
+         EVP_sha256(),               // digest
+     }},
+    {kms_v1::CryptoKeyVersion::RSA_DECRYPT_OAEP_4096_SHA256,
+     AlgorithmDetails{
+         kms_v1::CryptoKeyVersion::RSA_DECRYPT_OAEP_4096_SHA256,  // algorithm
+         kms_v1::CryptoKey::ASYMMETRIC_DECRYPT,                   // purpose
+         {CKM_RSA_PKCS_OAEP},        // allowed_mechanisms
+         CKK_RSA,                    // key_type
+         4096,                       // key_size
+         CKM_RSA_PKCS_KEY_PAIR_GEN,  // key_gen_mechanism
+         CKM_SHA256,                 // digest_mechanism
+         EVP_sha256(),               // digest
+     }},
+    {kms_v1::CryptoKeyVersion::RSA_DECRYPT_OAEP_4096_SHA512,
+     AlgorithmDetails{
+         kms_v1::CryptoKeyVersion::RSA_DECRYPT_OAEP_4096_SHA512,  // algorithm
+         kms_v1::CryptoKey::ASYMMETRIC_DECRYPT,                   // purpose
+         {CKM_RSA_PKCS_OAEP},        // allowed_mechanisms
+         CKK_RSA,                    // key_type
+         4096,                       // key_size
+         CKM_RSA_PKCS_KEY_PAIR_GEN,  // key_gen_mechanism
+         CKM_SHA512,                 // digest_mechanism
+         EVP_sha512(),               // digest
+     }},
 
-        // RSA_DECRYPT_OAEP_*
-        {KMS_ALGORITHM(RSA_DECRYPT_OAEP_2048_SHA256),
-         AlgorithmDetails{
-             KMS_ALGORITHM(RSA_DECRYPT_OAEP_2048_SHA256),  // algorithm
-             KMS_PURPOSE(ASYMMETRIC_DECRYPT),              // purpose
-             {CKM_RSA_PKCS_OAEP},                          // allowed_mechanisms
-             CKK_RSA,                                      // key_type
-             2048,                                         // key_size
-             CKM_RSA_PKCS_KEY_PAIR_GEN,                    // key_gen_mechanism
-             CKM_SHA256,                                   // digest_mechanism
-             EVP_sha256(),                                 // digest
-         }},
-        {KMS_ALGORITHM(RSA_DECRYPT_OAEP_3072_SHA256),
-         AlgorithmDetails{
-             KMS_ALGORITHM(RSA_DECRYPT_OAEP_3072_SHA256),  // algorithm
-             KMS_PURPOSE(ASYMMETRIC_DECRYPT),              // purpose
-             {CKM_RSA_PKCS_OAEP},                          // allowed_mechanisms
-             CKK_RSA,                                      // key_type
-             3072,                                         // key_size
-             CKM_RSA_PKCS_KEY_PAIR_GEN,                    // key_gen_mechanism
-             CKM_SHA256,                                   // digest_mechanism
-             EVP_sha256(),                                 // digest
-         }},
-        {KMS_ALGORITHM(RSA_DECRYPT_OAEP_4096_SHA256),
-         AlgorithmDetails{
-             KMS_ALGORITHM(RSA_DECRYPT_OAEP_4096_SHA256),  // algorithm
-             KMS_PURPOSE(ASYMMETRIC_DECRYPT),              // purpose
-             {CKM_RSA_PKCS_OAEP},                          // allowed_mechanisms
-             CKK_RSA,                                      // key_type
-             4096,                                         // key_size
-             CKM_RSA_PKCS_KEY_PAIR_GEN,                    // key_gen_mechanism
-             CKM_SHA256,                                   // digest_mechanism
-             EVP_sha256(),                                 // digest
-         }},
-        {KMS_ALGORITHM(RSA_DECRYPT_OAEP_4096_SHA512),
-         AlgorithmDetails{
-             KMS_ALGORITHM(RSA_DECRYPT_OAEP_4096_SHA512),  // algorithm
-             KMS_PURPOSE(ASYMMETRIC_DECRYPT),              // purpose
-             {CKM_RSA_PKCS_OAEP},                          // allowed_mechanisms
-             CKK_RSA,                                      // key_type
-             4096,                                         // key_size
-             CKM_RSA_PKCS_KEY_PAIR_GEN,                    // key_gen_mechanism
-             CKM_SHA512,                                   // digest_mechanism
-             EVP_sha512(),                                 // digest
-         }},
+    // RSA_SIGN_PKCS1_*
+    {kms_v1::CryptoKeyVersion::RSA_SIGN_PKCS1_2048_SHA256,
+     AlgorithmDetails{
+         kms_v1::CryptoKeyVersion::RSA_SIGN_PKCS1_2048_SHA256,  // algorithm
+         kms_v1::CryptoKey::ASYMMETRIC_SIGN,                    // purpose
+         {CKM_RSA_PKCS},             // allowed_mechanisms
+         CKK_RSA,                    // key_type
+         2048,                       // key_size
+         CKM_RSA_PKCS_KEY_PAIR_GEN,  // key_gen_mechanism
+         CKM_SHA256,                 // digest_mechanism
+         EVP_sha256(),               // digest
+     }},
+    {kms_v1::CryptoKeyVersion::RSA_SIGN_PKCS1_3072_SHA256,
+     AlgorithmDetails{
+         kms_v1::CryptoKeyVersion::RSA_SIGN_PKCS1_3072_SHA256,  // algorithm
+         kms_v1::CryptoKey::ASYMMETRIC_SIGN,                    // purpose
+         {CKM_RSA_PKCS},             // allowed_mechanisms
+         CKK_RSA,                    // key_type
+         3072,                       // key_size
+         CKM_RSA_PKCS_KEY_PAIR_GEN,  // key_gen_mechanism
+         CKM_SHA256,                 // digest_mechanism
+         EVP_sha256(),               // digest
+     }},
+    {kms_v1::CryptoKeyVersion::RSA_SIGN_PKCS1_4096_SHA256,
+     AlgorithmDetails{
+         kms_v1::CryptoKeyVersion::RSA_SIGN_PKCS1_4096_SHA256,  // algorithm
+         kms_v1::CryptoKey::ASYMMETRIC_SIGN,                    // purpose
+         {CKM_RSA_PKCS},             // allowed_mechanisms
+         CKK_RSA,                    // key_type
+         4096,                       // key_size
+         CKM_RSA_PKCS_KEY_PAIR_GEN,  // key_gen_mechanism
+         CKM_SHA256,                 // digest_mechanism
+         EVP_sha256(),               // digest
+     }},
+    {kms_v1::CryptoKeyVersion::RSA_SIGN_PKCS1_4096_SHA512,
+     AlgorithmDetails{
+         kms_v1::CryptoKeyVersion::RSA_SIGN_PKCS1_4096_SHA512,  // algorithm
+         kms_v1::CryptoKey::ASYMMETRIC_SIGN,                    // purpose
+         {CKM_RSA_PKCS},             // allowed_mechanisms
+         CKK_RSA,                    // key_type
+         4096,                       // key_size
+         CKM_RSA_PKCS_KEY_PAIR_GEN,  // key_gen_mechanism
+         CKM_SHA512,                 // digest_mechanism
+         EVP_sha512(),               // digest
+     }},
 
-        // RSA_SIGN_PKCS1_*
-        {KMS_ALGORITHM(RSA_SIGN_PKCS1_2048_SHA256),
-         AlgorithmDetails{
-             KMS_ALGORITHM(RSA_SIGN_PKCS1_2048_SHA256),  // algorithm
-             KMS_PURPOSE(ASYMMETRIC_SIGN),               // purpose
-             {CKM_RSA_PKCS},                             // allowed_mechanisms
-             CKK_RSA,                                    // key_type
-             2048,                                       // key_size
-             CKM_RSA_PKCS_KEY_PAIR_GEN,                  // key_gen_mechanism
-             CKM_SHA256,                                 // digest_mechanism
-             EVP_sha256(),                               // digest
-         }},
-        {KMS_ALGORITHM(RSA_SIGN_PKCS1_3072_SHA256),
-         AlgorithmDetails{
-             KMS_ALGORITHM(RSA_SIGN_PKCS1_3072_SHA256),  // algorithm
-             KMS_PURPOSE(ASYMMETRIC_SIGN),               // purpose
-             {CKM_RSA_PKCS},                             // allowed_mechanisms
-             CKK_RSA,                                    // key_type
-             3072,                                       // key_size
-             CKM_RSA_PKCS_KEY_PAIR_GEN,                  // key_gen_mechanism
-             CKM_SHA256,                                 // digest_mechanism
-             EVP_sha256(),                               // digest
-         }},
-        {KMS_ALGORITHM(RSA_SIGN_PKCS1_4096_SHA256),
-         AlgorithmDetails{
-             KMS_ALGORITHM(RSA_SIGN_PKCS1_4096_SHA256),  // algorithm
-             KMS_PURPOSE(ASYMMETRIC_SIGN),               // purpose
-             {CKM_RSA_PKCS},                             // allowed_mechanisms
-             CKK_RSA,                                    // key_type
-             4096,                                       // key_size
-             CKM_RSA_PKCS_KEY_PAIR_GEN,                  // key_gen_mechanism
-             CKM_SHA256,                                 // digest_mechanism
-             EVP_sha256(),                               // digest
-         }},
-        {KMS_ALGORITHM(RSA_SIGN_PKCS1_4096_SHA512),
-         AlgorithmDetails{
-             KMS_ALGORITHM(RSA_SIGN_PKCS1_4096_SHA512),  // algorithm
-             KMS_PURPOSE(ASYMMETRIC_SIGN),               // purpose
-             {CKM_RSA_PKCS},                             // allowed_mechanisms
-             CKK_RSA,                                    // key_type
-             4096,                                       // key_size
-             CKM_RSA_PKCS_KEY_PAIR_GEN,                  // key_gen_mechanism
-             CKM_SHA512,                                 // digest_mechanism
-             EVP_sha512(),                               // digest
-         }},
-
-        // RSA_SIGN_PSS_*
-        {KMS_ALGORITHM(RSA_SIGN_PSS_2048_SHA256),
-         AlgorithmDetails{
-             KMS_ALGORITHM(RSA_SIGN_PSS_2048_SHA256),  // algorithm
-             KMS_PURPOSE(ASYMMETRIC_SIGN),             // purpose
-             {CKM_RSA_PKCS_PSS},                       // allowed_mechanisms
-             CKK_RSA,                                  // key_type
-             2048,                                     // key_size
-             CKM_RSA_PKCS_KEY_PAIR_GEN,                // key_gen_mechanism
-             CKM_SHA256,                               // digest_mechanism
-             EVP_sha256(),                             // digest
-         }},
-        {KMS_ALGORITHM(RSA_SIGN_PSS_3072_SHA256),
-         AlgorithmDetails{
-             KMS_ALGORITHM(RSA_SIGN_PSS_3072_SHA256),  // algorithm
-             KMS_PURPOSE(ASYMMETRIC_SIGN),             // purpose
-             {CKM_RSA_PKCS_PSS},                       // allowed_mechanisms
-             CKK_RSA,                                  // key_type
-             3072,                                     // key_size
-             CKM_RSA_PKCS_KEY_PAIR_GEN,                // key_gen_mechanism
-             CKM_SHA256,                               // digest_mechanism
-             EVP_sha256(),                             // digest
-         }},
-        {KMS_ALGORITHM(RSA_SIGN_PSS_4096_SHA256),
-         AlgorithmDetails{
-             KMS_ALGORITHM(RSA_SIGN_PSS_4096_SHA256),  // algorithm
-             KMS_PURPOSE(ASYMMETRIC_SIGN),             // purpose
-             {CKM_RSA_PKCS_PSS},                       // allowed_mechanisms
-             CKK_RSA,                                  // key_type
-             4096,                                     // key_size
-             CKM_RSA_PKCS_KEY_PAIR_GEN,                // key_gen_mechanism
-             CKM_SHA256,                               // digest_mechanism
-             EVP_sha256(),                             // digest
-         }},
-        {KMS_ALGORITHM(RSA_SIGN_PSS_4096_SHA512),
-         AlgorithmDetails{
-             KMS_ALGORITHM(RSA_SIGN_PSS_4096_SHA512),  // algorithm
-             KMS_PURPOSE(ASYMMETRIC_SIGN),             // purpose
-             {CKM_RSA_PKCS_PSS},                       // allowed_mechanisms
-             CKK_RSA,                                  // key_type
-             4096,                                     // key_size
-             CKM_RSA_PKCS_KEY_PAIR_GEN,                // key_gen_mechanism
-             CKM_SHA512,                               // digest_mechanism
-             EVP_sha512(),                             // digest
-         }},
-    };
+    // RSA_SIGN_PSS_*
+    {kms_v1::CryptoKeyVersion::RSA_SIGN_PSS_2048_SHA256,
+     AlgorithmDetails{
+         kms_v1::CryptoKeyVersion::RSA_SIGN_PSS_2048_SHA256,  // algorithm
+         kms_v1::CryptoKey::ASYMMETRIC_SIGN,                  // purpose
+         {CKM_RSA_PKCS_PSS},         // allowed_mechanisms
+         CKK_RSA,                    // key_type
+         2048,                       // key_size
+         CKM_RSA_PKCS_KEY_PAIR_GEN,  // key_gen_mechanism
+         CKM_SHA256,                 // digest_mechanism
+         EVP_sha256(),               // digest
+     }},
+    {kms_v1::CryptoKeyVersion::RSA_SIGN_PSS_3072_SHA256,
+     AlgorithmDetails{
+         kms_v1::CryptoKeyVersion::RSA_SIGN_PSS_3072_SHA256,  // algorithm
+         kms_v1::CryptoKey::ASYMMETRIC_SIGN,                  // purpose
+         {CKM_RSA_PKCS_PSS},         // allowed_mechanisms
+         CKK_RSA,                    // key_type
+         3072,                       // key_size
+         CKM_RSA_PKCS_KEY_PAIR_GEN,  // key_gen_mechanism
+         CKM_SHA256,                 // digest_mechanism
+         EVP_sha256(),               // digest
+     }},
+    {kms_v1::CryptoKeyVersion::RSA_SIGN_PSS_4096_SHA256,
+     AlgorithmDetails{
+         kms_v1::CryptoKeyVersion::RSA_SIGN_PSS_4096_SHA256,  // algorithm
+         kms_v1::CryptoKey::ASYMMETRIC_SIGN,                  // purpose
+         {CKM_RSA_PKCS_PSS},         // allowed_mechanisms
+         CKK_RSA,                    // key_type
+         4096,                       // key_size
+         CKM_RSA_PKCS_KEY_PAIR_GEN,  // key_gen_mechanism
+         CKM_SHA256,                 // digest_mechanism
+         EVP_sha256(),               // digest
+     }},
+    {kms_v1::CryptoKeyVersion::RSA_SIGN_PSS_4096_SHA512,
+     AlgorithmDetails{
+         kms_v1::CryptoKeyVersion::RSA_SIGN_PSS_4096_SHA512,  // algorithm
+         kms_v1::CryptoKey::ASYMMETRIC_SIGN,                  // purpose
+         {CKM_RSA_PKCS_PSS},         // allowed_mechanisms
+         CKK_RSA,                    // key_type
+         4096,                       // key_size
+         CKM_RSA_PKCS_KEY_PAIR_GEN,  // key_gen_mechanism
+         CKM_SHA512,                 // digest_mechanism
+         EVP_sha512(),               // digest
+     }},
+};
 
 StatusOr<AlgorithmDetails> GetDetails(
     kms_v1::CryptoKeyVersion::CryptoKeyVersionAlgorithm algorithm) {
