cf82419e14465384b3ec591ae5ebdac3e3d9f6cc
Bradley Hess <bdhess@google.com>
2020-12-17 10:24:55 -0500

    Move Windows builds to Visual Studio 2019
    As a part of this change, also:
    - Stop hardcoding the full path to the Python executable, which
      changes with each point release.
    - Stop installing Bazel through Chocolatey. It's now included in the
      machine image.
    - Explicitly set the temp directory for builds to quiet up a new Bazel
      warning.
    
    Bug: b/160313555
    Change-Id: Ice1b425b32ba2fdf01b0223211cba2baccfa01ac

--- .kokoro/windows_build.bat
+++ .kokoro/windows_build.bat
@@ -9,15 +9,19 @@ cd "%PROJECT_ROOT%"
 set RESULTS_DIR=%KOKORO_ARTIFACTS_DIR%\results
 mkdir "%RESULTS_DIR%"
 
-:: Add the latest version of Bazel to the PATH
-choco install bazel --version 3.3.0 -y --no-progress || exit /b
-
 :: https://docs.bazel.build/versions/master/windows.html#build-c-with-msvc
-set BAZEL_VC=C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\
+set BAZEL_VC=C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\
 
 :: Force msys2 environment instead of Cygwin
 set PATH=C:\tools\msys64\usr\bin;%PATH%
 set BAZEL_SH=C:\tools\msys64\usr\bin\bash.exe
+
+:: Use our scratch drive for temp space
+mkdir T:\buildtmp
+set TMP=T:\buildtmp
+
+:: Ensure Bazel version information is included in the build log
+bazel version
 
 bazel test -c opt %BAZEL_EXTRA_ARGS% ... --keep_going
 set RV=%ERRORLEVEL%
@@ -26,7 +30,7 @@ if exist "%PROJECT_ROOT%\bazel-bin\kmsp11\main\libkmsp11.so" copy ^
     "%PROJECT_ROOT%\bazel-bin\kmsp11\main\libkmsp11.so" ^
     "%RESULTS_DIR%\kmsp11.dll"
 
-C:\Python37\python.exe "%PROJECT_ROOT%\.kokoro\copy_test_outputs.py" ^
+python "%PROJECT_ROOT%\.kokoro\copy_test_outputs.py" ^
     "%PROJECT_ROOT%\bazel-testlogs" "%RESULTS_DIR%\testlogs"
 
 exit %RV%
