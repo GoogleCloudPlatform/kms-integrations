c28766ca359167977e771da1da498d7577be41c1
Bradley Hess <bdhess@google.com>
2021-04-15 20:34:45 +0000

    Add asan and tsan build configurations
    Tests passed in Kokoro QA
    ASAN: http://shortn/_o4DnlRehan
    TSAN: http://shortn/_oBotpWeh3x
    
    Bug: b/160312010
    Change-Id: I0a30ebd0f3ee7bc4abfa30018d3d7b3951ff327d

---
+++ .kokoro/linux_build_san.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Fail on any error.
+set -e
+
+# Display commands being run.
+# WARNING: please only enable 'set -x' if necessary for debugging, and be very
+#  careful if you handle credentials (e.g. from Keystore) with 'set -x':
+#  statements like "export VAR=$(cat /tmp/keystore/credentials)" will result in
+#  the credentials being printed in build logs.
+#  Additionally, recursive invocation with credentials as command-line
+#  parameters, will print the full command, with credentials, in the build logs.
+# set -x
+
+# Code under repo is checked out to ${KOKORO_ARTIFACTS_DIR}/git.
+# The final directory name in this path is determined by the scm name specified
+# in the job configuration.
+export PROJECT_ROOT="${KOKORO_ARTIFACTS_DIR}/git/oss-tools"
+cd "${PROJECT_ROOT}"
+
+export RESULTS_DIR="${KOKORO_ARTIFACTS_DIR}/results"
+mkdir "${RESULTS_DIR}"
+
+# Pull in a more recent LLVM toolchain
+export LLVM_VERSION=10.0.1
+export LLVM_DIST="clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-ubuntu-16.04"
+export LLVM_ROOT=/opt/${LLVM_DIST}
+sudo tar xf "${KOKORO_GFILE_DIR}/${LLVM_DIST}.tar.xz" -C /opt
+
+echo "BAZEL_EXTRA_ARGS:"
+echo "${BAZEL_EXTRA_ARGS}"
+
+use_bazel.sh 4.0.0
+
+# Configure user.bazelrc with remote build caching options
+cp .kokoro/remote_cache.bazelrc user.bazelrc
+echo "build --remote_default_exec_properties=cache-silo-key=linux-san" >> user.bazelrc
+
+# Ensure that build outputs and test logs are uploaded even on failure
+_upload_artifacts() {
+  if [ -e "${PROJECT_ROOT}/bazel-bin/kmsp11/main/libkmsp11.so" ]; then
+    cp "${PROJECT_ROOT}/bazel-bin/kmsp11/main/libkmsp11.so" \
+      "${RESULTS_DIR}/libkmsp11.so"
+  fi
+
+  python3 "${PROJECT_ROOT}/.kokoro/copy_test_outputs.py" \
+    "${PROJECT_ROOT}/bazel-testlogs" "${RESULTS_DIR}/testlogs"
+}
+trap _upload_artifacts EXIT
+
+export CC=${LLVM_ROOT}/bin/clang
+export CPLUS_INCLUDE_PATH=${LLVM_ROOT}/include/c++/v1
+export BAZEL_CXXOPTS=-nostdinc++
+export BAZEL_LINKLIBS=-L${LLVM_ROOT}/lib:-lc++:-lc++abi:-Wl,-rpath=${LLVM_ROOT}/lib
+export SYMBOLIZER_PATH=${LLVM_ROOT}/bin/llvm-symbolizer
+
+bazel test ... \
+  --keep_going \
+  --test_env=ASAN_SYMBOLIZER_PATH=${SYMBOLIZER_PATH} \
+  --test_env=TSAN_SYMBOLIZER_PATH=${SYMBOLIZER_PATH} \
+  --test_env=UBSAN_SYMBOLIZER_PATH=${SYMBOLIZER_PATH} \
+  ${BAZEL_EXTRA_ARGS}
