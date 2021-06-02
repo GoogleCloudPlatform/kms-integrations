a76f5190b56bdd48050990a4ee5803a899673a62
Bradley Hess <bdhess@google.com>
2021-06-02 19:15:03 +0000

    Add license headers to source files
    This was done entirely automatically with
    `addlicense -c "Google LLC" -l apache .`
    
    Change-Id: I71882f2d8e5e549808e905416707e282594e48b4

--- kmsp11/util/pagination_range.h
+++ kmsp11/util/pagination_range.h
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // This file was ported from the Google Cloud Platform C++ Client Libraries,
 // the original source can be found here:
 // https://github.com/googleapis/google-cloud-cpp/blob/9d9591913f342bafcd83a34df8e96d4dd5d87534/google/cloud/internal/pagination_range.h
