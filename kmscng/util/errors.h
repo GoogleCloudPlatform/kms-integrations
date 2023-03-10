fd593897a676e935e7837026e12aa1e1d89c519b
Alessio Buraggina <aburaggina@google.com>
2023-03-10 13:27:56 -0500

    Add kmscng util and test helpers.
    Bug: b/266427457
    Bug: b/270415739
    Change-Id: I833a0a9332d92fb9aec11db5633c128e09987905

---
+++ kmscng/util/errors.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef KMSCNG_UTIL_ERRORS_H_
+#define KMSCNG_UTIL_ERRORS_H_
+
+#include <windows.h>
+
+#include "absl/status/status.h"
+#include "common/source_location.h"
+
+namespace cloud_kms::kmscng {
+
+// Creates a new error status with the provided parameters.
+// `code` and `ss` must not be OK; these requirements are CHECKed.
+absl::Status NewError(absl::StatusCode code, std::string_view msg,
+                      SECURITY_STATUS ss,
+                      const SourceLocation& source_location);
+
+// Creates a new Internal error with a SECURITY_STATUS of .
+inline absl::Status NewInternalError(std::string_view msg,
+                                     const SourceLocation& source_location) {
+  return NewError(absl::StatusCode::kInternal, msg, NTE_FAIL, source_location);
+}
+
+}  // namespace cloud_kms::kmscng
+
+#endif  // KMSCNG_UTIL_ERRORS_H_
