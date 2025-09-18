7b7ea8d5a65c6fcd0d5460c0ad606a9a9964cf3a
Alessio Buraggina <aburaggina@google.com>
2025-09-18 15:38:17 -0400

    Update results dir logic to reuse common kokoro config.
    Drop other logging statements too.
    
    Change-Id: I34270c3a6a61fbf8746614121762bab4621d939a

--- .kokoro/windows_sign.bat
+++ .kokoro/windows_sign.bat
@@ -10,25 +10,19 @@ cd "%KOKORO_GFILE_DIR%"
 :: Display dir contents for logging
 dir /s
 
-:: Create the standard output artifacts directory.
+:: Create the standard output results directory.
 :: Copy PKCS#11 and CNG artifacts to the output dir to sign them in place.
-mkdir "%KOKORO_ARTIFACTS_DIR%\artifacts"
-copy kmsp11.dll "%KOKORO_ARTIFACTS_DIR%\artifacts"
-copy kmscng.msi "%KOKORO_ARTIFACTS_DIR%\artifacts"
-cd "%KOKORO_ARTIFACTS_DIR%\artifacts"
+set RESULTS_DIR=%KOKORO_ARTIFACTS_DIR%\results
+mkdir "%RESULTS_DIR%"
+copy kmsp11.dll "%RESULTS_DIR%"
+copy kmscng.msi "%RESULTS_DIR%"
+cd "%RESULTS_DIR%"
 
 :: Attempt the signing for PKCS#11
 ksigntool.exe sign GOOGLE_EXTERNAL /v /debug /t http://timestamp.digicert.com "kmsp11.dll"
 
 :: Attempt the signing for CNG
 ksigntool.exe sign GOOGLE_EXTERNAL /v /debug /t http://timestamp.digicert.com "kmscng.msi"
-
-:: Display dir contents for logging
-dir /s
-
-:: Verify checks for logging
-ksigntool.exe verify /pa /all /debug "kmsp11.dll"
-ksigntool.exe verify /pa /all /debug "kmscng.msi"
 
 echo "All artifacts signed successfully!"
 
